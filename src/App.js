9327abce379b02ec98f5f921e598463009a0031c
gdejanovic <gorandejanovic@gmail.com>
2022-02-03 23:43:35 +0100

    added images and transition to cards

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import React from 'react'
-import Row from './Row';
-import requests from './request';
+import "./App.css";
+import React from "react";
+import Row from "./Row";
+import requests from "./request";
 function App() {
-
-
   return (
     <div className="App">
-      <h1>YO</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending now" fetchUrl={requests.fetchTopRated}/>
+      <h1>Netflix clone</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending now" fetchUrl={requests.fetchTopRated} />
     </div>
   );
 }
 
 export default App;
-
- 
--- src/Row.js
+++ src/Row.js
@@ -1,31 +1,38 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import axios from './axios';
+import axios from "./axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
-    const [movies, setMovies] = useState([]);
-    useEffect(() =>{
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            console.log(request);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    },[fetchUrl]);
-// run once and never load it again, if there is a variable code runs again
-    
-    return (
-        <div>
-            <h2>{title}</h2>
-          
-            {/* container - posters */}
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.table(movies);
+  // run once and never load it again, if there is a variable code runs again
 
-        </div>
-
-    )
-
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+            className="row__poster"
+          />
+        ))}
+      </div>
+      {/* container - posters */}
+    </div>
+  );
 }
 
 export default Row;
