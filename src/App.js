2459fd9a262e7b37d8f69d4bf18673681ba14df4
gdejanovic <gorandejanovic@gmail.com>
2022-02-03 22:21:26 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react'
+import Row from './Row';
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>YO</h1>
+      <Row title="NETFLIX ORIGINALS"/>
+      <Row title="Trending now"/>
+    </div>
+  );
+}
+
+export default App;
+
+ 
