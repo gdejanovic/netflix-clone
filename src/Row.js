77ae86b6c2b50edf42161e562adcdc8743643e66
gdejanovic <gorandejanovic@gmail.com>
2022-02-03 22:56:40 +0100

    added api connection

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,21 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import axios from './axios';
 
-function Row({title}) {
+function Row({title, fetchUrl}) {
+    const [movies, setMovies] = useState([]);
+    useEffect(() =>{
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            console.log(request);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    },[fetchUrl]);
+// run once and never load it again, if there is a variable code runs again
+    
     return (
         <div>
             <h2>{title}</h2>
--- src/request.js
+++ src/request.js
@@ -1,14 +1,9 @@
 const APIKEY = 'fe81a7590ed309e5d50f9bb6b4ccfc00';
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
-    fetchActionMovies: `/movie?api_key=${APIKEY}&language=en-US`,
 
 }
 
-export default function request (){
-
-
-
-}
+export default requests;
