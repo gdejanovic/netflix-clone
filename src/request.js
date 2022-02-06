73e21ff075c5a397bc4cf0a5de7d8ca77a9ae8d0
gdejanovic <gorandejanovic@gmail.com>
2022-02-06 21:49:37 +0100

    added more categories

--- src/request.js
+++ src/request.js
@@ -1,8 +1,13 @@
 const APIKEY = "fe81a7590ed309e5d50f9bb6b4ccfc00";
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_network=123`,
   fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`,
 };
 
 export default requests;
