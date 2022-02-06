a2a28d88a1222168a1e1c16f8539355946e15ad2
gdejanovic <gorandejanovic@gmail.com>
2022-02-06 22:58:45 +0100

    deployed version to the firebase

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
-const base_url = "https://image.tmdb.org/t/p/original/";
+
+const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -18,17 +20,20 @@ function Row({ title, fetchUrl, isLargeRow }) {
     <div className="row">
       <h2>{title}</h2>
       <div className="row_posters">
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+        {movies.map((movie) => {
+          return (
+            <img
+              key={movie.id}
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          );
+        })}
       </div>
+      <div style={{ padding: "40px" }}></div>
     </div>
   );
 }
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
+
+/** base url to make request to the themoviedatabase */
 
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
+	baseURL: 'https://api.themoviedb.org/3'
 });
+
+// instance.get('/foo-bar');
+// https://api.themoviedb.org/3/foo-bar
 
 export default instance;
